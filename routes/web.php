83a61005a4579953feeef849176001f0760e952a
Fabian Munoz <luisfa-bi9@hotmail.com>
2018-06-28 04:59:57 +0100

    incluyendo api enpoint

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
-
+$api = app('Dingo\Api\Routing\Router');
 
 $api->version('v1', function ($api) {
     $api->get('users/{id}', 'App\Api\Controllers\UserController@show');
